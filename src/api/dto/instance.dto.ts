127b9debd86278e145cc38c253db45cb993bad04
zaphyroxx <josedanielaj.201@gmail.com>
2025-07-05 10:31:32 -0500

    Update instance.dto.ts

--- src/api/dto/instance.dto.ts
+++ src/api/dto/instance.dto.ts
@@ -14,6 +14,7 @@ export class InstanceDto extends IntegrationDto {
   ownerJid?: string;
   profileName?: string;
   profilePicUrl?: string;
+
   // settings
   rejectCall?: boolean;
   msgCall?: string;
@@ -23,13 +24,16 @@ export class InstanceDto extends IntegrationDto {
   readStatus?: boolean;
   syncFullHistory?: boolean;
   wavoipToken?: string;
+
   // proxy
   proxyHost?: string;
   proxyPort?: string;
   proxyProtocol?: string;
   proxyUsername?: string;
   proxyPassword?: string;
-  webhook?: {
+
+  // propiedades heredadas de IntegrationDto (se usa 'declare' para evitar TS2612)
+  declare webhook: {
     enabled?: boolean;
     events?: string[];
     headers?: JsonValue;
@@ -37,20 +41,20 @@ export class InstanceDto extends IntegrationDto {
     byEvents?: boolean;
     base64?: boolean;
   };
-  chatwootAccountId?: string;
-  chatwootConversationPending?: boolean;
-  chatwootAutoCreate?: boolean;
-  chatwootDaysLimitImportMessages?: number;
-  chatwootImportContacts?: boolean;
-  chatwootImportMessages?: boolean;
-  chatwootLogo?: string;
-  chatwootMergeBrazilContacts?: boolean;
-  chatwootNameInbox?: string;
-  chatwootOrganization?: string;
-  chatwootReopenConversation?: boolean;
-  chatwootSignMsg?: boolean;
-  chatwootToken?: string;
-  chatwootUrl?: string;
+  declare chatwootAccountId: string;
+  declare chatwootConversationPending: boolean;
+  declare chatwootAutoCreate: boolean;
+  declare chatwootDaysLimitImportMessages: number;
+  declare chatwootImportContacts: boolean;
+  declare chatwootImportMessages: boolean;
+  declare chatwootLogo: string;
+  declare chatwootMergeBrazilContacts: boolean;
+  declare chatwootNameInbox: string;
+  declare chatwootOrganization: string;
+  declare chatwootReopenConversation: boolean;
+  declare chatwootSignMsg: boolean;
+  declare chatwootToken: string;
+  declare chatwootUrl: string;
 }
 
 export class SetPresenceDto {
